fee05070f316778bff250b75e972280bd9488430
MadhanBharathiK <143484679+MadhanBharathiK@users.noreply.github.com>
2024-04-18 12:06:04 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,4 +2,5 @@ public class Hello{
   public static void main(String[] args) {
     System.out.println("Hey Testing");
     System.out.println("Hey jenkins");
+    System.out.println("Hey Examples");
     
